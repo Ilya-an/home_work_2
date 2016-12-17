6d15a4f13dc0fafc66a015fb22afd0effa98d062
Илья <ilya@localhost.localdomain>
2016-12-17 15:47:03 +0300

    first commit

---
+++ modem.cpp
@@ -0,0 +1,74 @@
+#include "modem.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Modem::Modem (const string name, const string type, const interface connector, const string manufacturer, const int price,const bool flag):
+{
+    setName (name);
+    setType (type);
+    setConn (connector);
+    setManufacturer (manufacturer);
+    setPrice (price);
+    setBool (flag);
+}
+void Modem::setType (const string type)
+{
+    this->type = type;
+}
+
+void Modem::setConn (const interface conn)
+{
+    connector = conn;
+}
+void Modem::setName (const string name)
+{
+    this->name = name;
+}
+
+void Modem::setManufacturer (const string manufacturer)
+{
+    this->manufacturer = manufacturer;
+}
+
+void Modem::setPrice (const int price)
+{
+    this->price = price;
+}
+void Modem::setBool (const bool flag)
+{
+    internalisers = flag;
+}
+
+Modem::~Modem ()
+{
+
+}
+
+ostream& operator<<(ostream& stream, const Modem& mod)
+{
+    stream << "  1. Название модема: " << mod.name << endl;
+    stream << "  2. Производитель модема: " << mod.manufacturer << endl;
+    stream << "  3. Тип модема: " <<  mod.type << endl;
+    stream << "  4. Стоимость модема: " << mod.price << endl;
+    string integral = "";
+    switch (mod.getBool ())
+    {
+        case true: integral = "Да"; break;
+        case false: integral = "Нет"; break;
+    }
+    stream << "  5. Встроенный модем: " << integral << endl;
+    string Conn = "";
+    switch (mod.getConn())
+    {
+        case 1: Conn = "COM"; break;
+        case 2: Conn = "ExpressCard"; break;
+        case 3: Conn = "PSI"; break;
+        case 4: Conn = "PCMCIA"; break;
+        case 5: Conn = "USB"; break;
+    }
+    stream << "  6. Интерфейс модема: " << Conn << endl;
+
+    return stream;
+}
---
+++ modem.hpp
@@ -0,0 +1,43 @@
+#ifndef MODEM_HPP_INCLUDED
+#define MODEM_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Modem
+{
+    string name;
+    string manufacturer;  //ABIT,ACORP,ASUSTEK,GIGABITE,INTEL,ELITEGROUP
+    string type;// (Pentium,PII,PIII,Athlon) Поколение процессора
+    int price; // BabyAT=1,AT,ATX,WTX
+    bool internalisers; //  Интегрированная ли плата или нет (т.е. со встроенной звуковой/видео картой)
+    public:
+        enum interface
+        {
+            COM = 1, ExpressCard, PSI, PCMCIA, USB
+        };
+        interface connector;
+        Modem ();
+        Modem (const string name, const string type, const interface connector, const string manufacturer, const int price, const bool flag);
+        ~Modem();
+        void setName (const string name);
+        void setManufacturer ( const string manufacturer);
+        void setType (const string type);
+        void setConn (const interface conn);
+        void setPrice (const int price);
+        void setBool (const bool flag);
+
+
+        string getName () const {return name;}
+        string getType () const {return type;}
+        interface getConn () const {return connector;}
+        string getManufacturer () const {return manufacturer;}
+        int getPrice () const {return price;}
+        bool getBool () const {return internalisers;}
+
+        friend ostream& operator << (ostream& os, const Modem& mom);
+};
+
+#endif // MODEM_HPP_INCLUDED
